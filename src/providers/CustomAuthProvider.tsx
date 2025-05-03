0aafe4762bf7826215af290ae431823c5bd72175
tareq.rar <tareq.rar@gmail.com>
2025-05-03 15:53:16 +0300

    Add Next.js configuration file with TypeScript support and internationalization
    - Introduced next.config.ts to configure Next.js settings, enabling React strict mode and SWC minification.
    - Set up image domains and SASS options for styling.
    - Configured TypeScript to ignore build errors in production.
    - Added internationalization support for English and Arabic locales.

--- src/providers/CustomAuthProvider.tsx
+++ src/providers/CustomAuthProvider.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Loader from '@components/layout/Loader';
-// import useUser from '@hooks/useUser';
 
 interface CustomAuthProviderProps {
   children: React.ReactNode;
