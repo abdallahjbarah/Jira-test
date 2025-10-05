623c48e47f3afdfc02b3180c3e9aa7777320bfcd
hamza ndeal alhasan <hamza.alhasan29@gmail.com>
2025-10-05 17:00:06 +0300

    fixing persist

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import React from 'react';
+import { reactQueryClientOptions } from '@configs/reactQueryClientOptions';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { reactQueryClientOptions } from '@configs/reactQueryClientOptions';
 import { persistQueryClient } from '@tanstack/react-query-persist-client';
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import React from 'react';
 
 interface ReactQueryProviderProps {
   children: React.ReactNode;
@@ -25,9 +25,15 @@ function ReactQueryProvider({
       persistQueryClient({
         queryClient,
         persister: localStoragePersister,
+        dehydrateOptions: {
+          shouldDehydrateQuery: query => {
+            const queryKey = query.queryKey[0] as string;
+            return queryKey === 'user' || queryKey === 'bookingData';
+          },
+        },
       });
     }
-  }, [queryClient]);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
