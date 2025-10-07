bd3cefa7d3ef4cf28aa7ff0a677d809fa509978b
hamza ndeal alhasan <hamza.alhasan29@gmail.com>
2025-10-07 10:45:57 +0300

    fixing logout issues

--- src/configs/reactQueryClientOptions.ts
+++ src/configs/reactQueryClientOptions.ts
@@ -1,10 +1,9 @@
-import { ONE_MINUTE_IN_MILLI } from '@utils/constants';
-
 export const reactQueryClientOptions = {
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      gcTime: ONE_MINUTE_IN_MILLI * 30,
+      // gcTime: ONE_MINUTE_IN_MILLI * 30,
+      gcTime: Infinity,
       retry: 0,
     },
   },
